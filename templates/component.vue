76be319727538f71ceadf6bdd145d415b7a6fe6b
Pendar <pendar747@gmail.com>
2017-09-24 23:14:10 +0100

    component template

--- templates/component.vue
+++ templates/component.vue
@@ -1,14 +1,19 @@
 <template>
-  <div></div>
+    <div></div>
 </template>
 
-<script>
-export default {
-  data () {
-    return {}
-  }
+<script lang='ts'>
+
+import Vue from 'vue';
+import Component from 'vue-class-component';
+import Error404 from './Error404.vue';
+
+@Component
+export default class Component extends Vue {
 }
 </script>
 
-<style>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus">
+    
 </style>
